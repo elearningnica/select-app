b9d5b36b4244810b83fcf7670cd6035310d06cb3
Milton Navarro <javier.desarrollador@hotmail.com>
2021-08-10 14:27:41 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import { Select } from 'antd';
+import {data} from './data';
+import React, {useState} from 'react';
+
 
 function App() {
-  return (
+
+  const [countries, setCountries] = useState(data);
+
+  const {Option} = Select;
+
+  function handleChange(value){
+    console.log(`selected ${value}`); //show the selected items in the console
+  }
+
+  return <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Select mode="multiple"
+      style={{ width: '100%'}}
+      placeholder="select one country"
+      onChange={handleChange}
+      optionLabelProp="label">
+      {
+        countries.map((country) => {
+          const {id, name} = country;
+          return (
+            <Option value={name} label={name}>
+              <div className="demo-option-label-item">
+                <span role="img" aria-label={name}>{name}</span>
+              </div>
+            </Option>
+          );
+        })        
+      }
+      </Select>
+      
     </div>
-  );
+  </>
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const data = [
+  { id: 1, name: 'USA' },
+  { id: 2, name: 'Germany' },
+  { id: 3, name: 'France' },
+  { id: 4, name: 'Canada' },
+  { id: 5, name: 'Brazil' },
+  { id: 6, name: 'Argentina' },
+  { id: 7, name: 'Spain' },
+  { id: 8, name: 'Russia' },
+  { id: 9, name: 'Mexico' },
+  { id: 10, name: 'England' },
+];
